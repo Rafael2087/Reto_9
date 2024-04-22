cf50c20f52edeb8bec45597ab9eaeadc53bc88a0
Rafael2087 <159049583+Rafael2087@users.noreply.github.com>
2024-04-22 18:25:32 -0500

    Funciones Lamda

--- Punto_1.py
+++ Punto_1.py
@@ -1,13 +1,23 @@
-def factorialRecursivo(n : int )-> int:
-  print(n)
-  # Caso base 
-  if n == 1: 
-    return 1
-  else:
-    # Condicion de la funcion recursiva
-    return n*factorialRecursivo(n-1)
+if __name__ == "__main__": # Se va a tomar el punto 1 del Reto 8 #
+    print(f"Ejercicio 1 del reto 8")
+    i : int = int(input("DIjite un número: "))
+    CuadradoFuncion = lambda x: x**2 # Esta función nos permite calcular el cuadrado de un número #
+    Cuadrado = CuadradoFuncion(i)
+    print(f"El número {i} elevado al cuadrado es igual a {Cuadrado}")
 
-if __name__ == "__main__":
-  num = int(input("Ingrese numero: "))
-  factorialDeNum = factorialRecursivo(num)
-  print("El factorial de " + str(num) + " es " + str(factorialDeNum))
+    # Se va a tomar el punto 6 del reto 6 #
+    print(f"Ejercicio 6 del reto 6")
+    contagiados_inicial : int = int(input("Dijite la cantidad de contagiados inicialmente: "))
+    dias : int = int(input("Dijite el número de días transcurridos: "))
+    ContagiadosFuncion = lambda x , y : x * 2 ** y # Se define la función lambda para calcular el número de contagiados #
+    Contagiados = ContagiadosFuncion(contagiados_inicial,dias)
+    print(f"El número de contagiados despues de {dias} días es igual a {Contagiados}")
+
+    # Se va a tomar el punto 5 del reto 6 #
+    print(f"Ejercicio 5 del reto 6")
+    capital_inicial : float = float(input("Dijite el capital inicial: "))
+    porcentaje_interes : float = float(input("Dijite el porcentaje de interes (%): "))
+    meses : int = int(input("Dijite la cantidad de meses: "))
+    InteresFuncion = lambda x, y, z : x*(1 + y/100)**(z)
+    Interes = InteresFuncion(capital_inicial,porcentaje_interes,meses)
+    print(f"El valor total a pagar es de {Interes} $")
